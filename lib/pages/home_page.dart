248a16e575d44b970164a6cf25ddf0b427792cff
Moksh01 <shahmoksh001@gmail.com>
2023-10-29 14:08:24 +0530

    day13

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,16 +1,35 @@
 import "package:flutter/material.dart";
+import "package:flutter/services.dart";
 import "package:flutter_catalog/models/catalog.dart";
 import "package:flutter_catalog/widgets/drawer.dart";
 import "package:flutter_catalog/widgets/item_widget.dart";
+import 'dart:convert';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final dummyList = List.generate(20, (index) => CatalogModels.items[0]);
     int days = 30;
     String name = "moksh";
+    loadData() async {
+      var catalogJson = await rootBundle.loadString("assets/file/catalog.json");
+      var decodeData = jsonDecode(catalogJson);
+      var productsData = decodeData["products"];
+      print(productsData);
+    }
+
+    void initState() {
+      super.initState();
+      loadData();
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
