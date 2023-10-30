bf93d4864431ecfbba245620a14fa1c3e1a2a131
Moksh01 <shahmoksh001@gmail.com>
2023-10-30 10:32:15 +0530

    day-16

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -39,16 +39,45 @@ class _HomePageState extends State<HomePage> {
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items !=null && CatalogModel.items.isNotEmpty)?  ListView.builder(
-          itemCount: CatalogModel.items.length,
-          itemBuilder: (context, index) {
-            return ItemWidgets(
-              item: CatalogModel.items[index],
-            );
-          },
-        ): Center(child: CircularProgressIndicator(),)
-      ),
+          padding: const EdgeInsets.all(16.0),
+          child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
+              ? GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 16,
+                    mainAxisSpacing: 16,
+                  ),
+                  itemBuilder: (context, index) {
+                    final item = CatalogModel.items[index];
+                    return Card(
+                        clipBehavior: Clip.antiAlias,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10)),
+                        child: GridTile(
+                          header: Container(
+                            child: Text(
+                              item.name,
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            padding: EdgeInsets.all(12),
+                            decoration: BoxDecoration(color: Colors.red),
+                          ),
+                          child: Image.network(item.image),
+                          footer: Container(
+                            child: Text(
+                              item.price.toString(),
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            padding: EdgeInsets.all(12),
+                            decoration: BoxDecoration(color: Colors.black),
+                          ),
+                        ));
+                  },
+                  itemCount: CatalogModel.items.length,
+                )
+              : Center(
+                  child: CircularProgressIndicator(),
+                )),
       drawer: MyDrawer(),
     );
   }
