861f95e3c1eed3291f84606297a7b5d5371b2e4d
Moksh01 <shahmoksh001@gmail.com>
2023-11-01 03:14:54 +0530

    day26

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -4,10 +4,6 @@ import 'dart:convert';
 import 'package:flutter/foundation.dart';
 
 class CatalogModel {
-  static final catModel = CatalogModel._internal();
-  CatalogModel._internal();
-  factory CatalogModel() => catModel;
-
   static List<Item> items = [];
   //get item by id
   Item getById(int id) =>
