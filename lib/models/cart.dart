7958855cbe54ad574bcfb6bdec0089d892c99c26
Moksh01 <shahmoksh001@gmail.com>
2023-11-01 11:32:44 +0530

    day27

--- lib/models/cart.dart
+++ lib/models/cart.dart
@@ -1,4 +1,6 @@
+import 'package:flutter_catalog/core/store.dart';
 import 'package:flutter_catalog/models/catalog.dart';
+import 'package:velocity_x/velocity_x.dart';
 
 class CartModel {
   //catalog fields
@@ -17,13 +19,18 @@ class CartModel {
   //Get total price
   num get totalPrice =>
       items.fold(0, (total, current) => total + current.price);
-  //add item
-  void add(Item item) {
-    _ItemIds.add(item.id);
-  }
 
   //remove item
   void remove(Item item) {
     _ItemIds.remove(item.id);
+  }
+}
+
+class AddMutation extends VxMutation<MyStore> {
+  final Item item;
+  AddMutation(this.item);
+  @override
+  perform() {
+    store!.cart._ItemIds.add(item.id);
   }
 }
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:flutter_catalog/core/store.dart";
 import "package:flutter_catalog/models/cart.dart";
 import "package:flutter_catalog/widgets/theme.dart";
 import "package:velocity_x/velocity_x.dart";
@@ -24,11 +25,11 @@ class CartPage extends StatelessWidget {
 }
 
 class _CartTotal extends StatelessWidget {
-  final _cart = CartModel();
   _CartTotal({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final CartModel _cart = (VxState.store as MyStore).cart;
     return SizedBox(
       height: 200,
       child: Row(
@@ -57,9 +58,9 @@ class _CartTotal extends StatelessWidget {
 }
 
 class _CartList extends StatelessWidget {
-  final _cart = CartModel();
   @override
   Widget build(BuildContext context) {
+    final CartModel _cart = (VxState.store as MyStore).cart;
     return _cart.items!.isEmpty
         ? "Nothing to show".text.xl3.makeCentered()
         : ListView.builder(
